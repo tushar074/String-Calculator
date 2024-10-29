8cbc68ff83bf93a7aeacde3d814a8ae73c17bcbf
tushar074 <tushar.asansol@gmail.com>
2024-10-30 01:16:39 +0530

    Create StringCalculator class with add method placeholder

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -4,10 +4,9 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
+public class StringCalculator 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public int add(String numbers) {
+        return 0; // Placeholder return for the initial test
     }
 }
